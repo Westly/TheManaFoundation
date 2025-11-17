c4e4dcec69a6ef687a9a88d824a76cd40c1bbf8d
Westly <westlywhite@gmail.com>
2025-11-16 20:52:12 -0700

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,26 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  eleventyConfig.addFilter("isoDate", function (dateObj) {
+    if(!dateObj) return "";
+    try {
+      return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-MM-dd");
+    } catch(e) {
+      return "";
+    }
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
